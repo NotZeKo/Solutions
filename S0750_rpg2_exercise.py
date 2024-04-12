3069c26754d09ef7458677dd37c16127f093f307
NotZeKo <emilsommerly@hotmail.com>
2024-04-12 14:20:56 +0200

    S0100_Test completed

--- S0750_rpg2_exercise.py
+++ S0750_rpg2_exercise.py
@@ -31,23 +31,36 @@ Når dit program er færdigt, skal du skubbe det til dit github-repository.
 Send derefter denne Teams-besked til din lærer: <filename> done
 Fortsæt derefter med den næste fil."""
 
+import random
+
+
+class Color:
+    RESET = "\033[0m"
+    RED = "\033[91m"
+    GREEN = "\033[92m"
+    BLUE = "\033[94m"
+    GREY = "\033[90m"
+    DARK_RED = "\033[31m"
+    ORANGE = "\033[38;5;208m"
+
+
 class Character:
-    def __init__(self, name, max_health, _current_health, attackpower, heal_power=0, turn=1):
+    def __init__(self, name, max_health, _current_health, attackpower=0, heal_power=0, fire_damage=0):
         self.name = name
         self.max_health = max_health
         self._current_health = _current_health
         self.attackpower = attackpower
         self.heal_power = heal_power
-        self.turn = turn
-
-
+        self.fire_damage = fire_damage
 
     def __repr__(self):
-        return f'Turn: {self.turn}\nName: {self.name}\nCurrent Health: {self._current_health}\\{self.max_health}\nAttackpower: {self.attackpower}\n'
+        return f'Name: {Color.GREY}{self.name}{Color.RESET}\nCurrent Health:{Color.GREEN} {self._current_health}\\{self.max_health}{Color.RESET}\nAttackpower: {Color.DARK_RED}{self.attackpower}{Color.RESET}\nFire power: {Color.ORANGE}{self.fire_damage}{Color.RESET}\n'
 
     def hit(self, other):
-        print(f'{self.name} hit {other.name} for {self.attackpower}DMG\n')
+        self.attackpower = random.randint(10, 15)
+        print(f'{Color.GREEN}{self.name}{Color.RESET} hit {other.name} for {Color.DARK_RED}{self.attackpower} Physical{Color.DARK_RED} DMG{Color.RESET}\n')
         other.get_hit(self.attackpower)
+
     def get_hit(self, attackpower):
         self._current_health -= attackpower
 
@@ -58,41 +71,83 @@ class Character:
             print(f"{self.name} is at max health and can't heal further\n")
             self._current_health = self.max_health
 
+    def fire_hit(self, amount):
+        print(f'{self.name} took {amount} fire damage')
+        self._current_health -= amount
+
+    def fight_hit(self, amount):
+        print(f'{self.name} took {amount} fighting damage')
+        self._current_health -= amount
+
     def dead(self):
         return self._current_health <= 0
 
+
 class Healer(Character):
-    def __init__(self, name, max_health, _current_health, attack_power, heal_power):
-        super().__init__(name, max_health, _current_health, attack_power,)
+    def __init__(self, name, max_health, _current_health, attackpower, heal_power):
+        super().__init__(name, max_health, _current_health, attackpower, )
         self.heal_power = heal_power
 
     def heal(self, character_heal):
         character_heal.get_healed(self.heal_power)
 
+
+class Mage(Character):
+    def __init__(self, name, max_health, _current_health, attackpower=0, fire_damage=0):
+        super().__init__(name, max_health, _current_health, attackpower)
+        self.fire_damage = fire_damage
+
+    def fire_attack(self, other):
+        self.fire_damage = random.randint(10,15)
+        print(f'{Color.GREEN}{self.name}{Color.RESET} hit {other.name} for {Color.ORANGE}{self.fire_damage} Fire DMG{Color.RESET}\n')
+        other.fire_hit(self.fire_damage)
+
+class Brawler(Character):
+    def __init__(self, name, max_health, _current_health, attackpower=0, fight_damage=0):
+        super().__init__(name, max_health, _current_health, attackpower)
+        self.fight_damage = fight_damage
+
+    def fight_attack(self, other):
+        self.fight_damage = random.randint(10, 15)
+        print(f'{Color.GREEN}{self.name}{Color} hit {other.name} for {Color.RED} Fighting DMG{Color.RESET}\n')
+        other.fight_hit(self.fight_attack)
+
 # kaneki = Healer("Kaneki", 120, 120, 0, 20)
-tara = Character("Tara", 100, 100, 10)
-masuke = Character("Masuke", 130, 130, 13)
-
+tara = Mage('Tara', 100, 100)
+masuke = Brawler('Masuke', 100, 100)
+# tara = Character("Tara", 100, 100, random.randint(10, 15))
+# masuke = Character("Masuke", 100, 100, random.randint(10, 15))
+turn = 0
+fire_cooldown = 0 #Lav videre på cooldown
+tara_win = 0
+masuke_win = 0
 print(tara)
-while tara.turn < 10:
-    if tara.turn % 2 == 0:
-        tara.turn += 1
-        tara.hit(masuke)
-        print(tara)
-    else:
+print(masuke)
+while turn < 1000:
+    turn += 1
+    print(f"Turn: {Color.BLUE}{turn}{Color.RESET}\n")
+    if tara.dead():
+        print(f"{Color.RED}Tara has died in turn: {turn}{Color.RESET}")
+        masuke_win += 1
+        print(f"Masuke has won {masuke_win} times")
+        tara._current_health = tara.max_health
+        masuke._current_health = masuke.max_health
+
+    if masuke.dead():
+        print(f"{Color.RED}Masuke has died in turn: {turn}{Color.RESET}")
+        tara_win += 1
+        print(tara_win)
+        masuke._current_health = masuke.max_health
+        tara._current_health = tara.max_health
+
+    attackpower = random.randint(10, 20)
+
+    if turn % 2 == 0:
+        tara.fire_attack(masuke)
         print(masuke)
-        tara.turn += 1
+    else:
+        masuke.hit(tara)
+        print(tara)
 
-
-
-
-
-# print(kaneki)
-# while not kaneki.dead() and kaneki.turn < 100:
-#     kaneki.turn +=1
-#     print(kaneki)
-#     if tara._current_health <= 80:
-#         kaneki.heal(tara)
-#     elif kaneki._current_health <= 100:
-#         kaneki.heal(kaneki)
-# print(tara)
+if turn == 1000:
+    print(f"After {Color.BLUE}{turn}{Color.RESET} turns\n{Color.RED}Masuke{Color.RESET} has won {Color.GREEN}{masuke_win}{Color.RESET} times\n{Color.RED}Tara{Color.RESET} has won {Color.GREEN}{tara_win}{Color.RESET} times")
