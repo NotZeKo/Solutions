3ca1513773c97a45777ad589caa826e003909417
NotZeKo <emilsommerly@hotmail.com>
2024-04-23 14:04:32 +0200

    S0100_Test completed

--- Fun/S1510_turtle_hunt_classes_constants.py
+++ Fun/S1510_turtle_hunt_classes_constants.py
@@ -78,36 +78,78 @@ import random
 from S1520_turtle_hunt_service import distance, direction
 
 
-class PlayerName1(turtle.Turtle):
+class Prey(turtle.Turtle):
 
     def __init__(self):
         super().__init__()  # Here, this is equivalent to turtle.Turtle.__init__(self)
         self.orientation = 0  # used to keep track of the turtle's current orientation (the direction it is heading)
 
     def rotate_prey(self, positions):  # turtle will be turned right <degree> degrees. Use negative values for left turns.
-        # self: the turtle that shall be rotated
-        # positions: a list of tuples. Each tuple is a pair of coordinates (x,y).
-        # positions[0] is the coordinate tuple of the prey. positions[0][0] is the x-coordinate of the prey.
-        # positions[1], positions[2], positions[3] refer to the hunters.
-        # for example is positions[3][1] the y-coordinate of the third hunter.
+        degree = 8
+        prey_position = positions[0]
+        hunters_positions = positions[1:]
+        hunter_within_range = False
+        hunter0 = True
+        hunter1 = True
+        hunter2 = True
+        for hunter_position in hunters_positions:
 
-        # Example for use of the service functions distance() and direction
-        # print(f'{distance(positions[0], positions[1])=}   {direction(positions[0], positions[1])=}')  # print distance and direction from prey to hunter1
+            if distance(hunters_positions[0], prey_position) < 100:
+                print("Hunter 0")
+                if hunter0:
+                    degree += direction(hunters_positions[0], prey_position) + 180
+                    if degree > 360:
+                        degree -= 360
+                    hunter0 = False
+                self.setheading(degree)
+                self.forward(3)
 
-        degree = 3  # When the turtle rotates the same amount each turn,  it will just run in a circle. Make this function smarter!
-        self.orientation += degree
-        self.orientation %= 360
-        # print(self.orientation)
+            if distance(hunters_positions[1], prey_position) < 50:
+                print("Hunter 1")
+                if hunter1:
+                    degree += direction(hunters_positions[0], prey_position) + 180
+                    if degree > 360:
+                        degree -= 360
+                    hunter1 = False
+                self.setheading(degree)
+                self.forward(3)
+
+            if distance(hunters_positions[2], prey_position) < 50:
+                print("Hunter 2")
+                if hunter2:
+                    degree += direction(hunters_positions[0], prey_position) + 180
+                    if degree > 360:
+                        degree -= 360
+                    hunter2 = False
+                self.setheading(degree)
+                self.forward(3)
+
+
+        if not hunter_within_range:
+            self.orientation += degree
+            self.orientation %= 360
+            # print(self.orientation)
+            # print(f'{direction(hunter_position, prey_position)}')
+
         return degree
+
+        # turn = 0
+        # degree = 8  # When the turtle rotates the same amount each turn,  it will just run in a circle. Make this function smarter!
+        # self.orientation += degree
+        # self.orientation %= 360
+        # return degree
 
     def rotate_hunter(self, positions):  # turtle will be turned right <degree> degrees. Use negative values for left turns.
         # Example for use of the service functions distance() and direction
         # print(f'{distance(self.position(), positions[0])=}   {direction(self.position(), positions[0])=}')  # print distance and direction from the current hunter to the prey
-        degree = -0.5  # When the turtle rotates the same amount each turn,  it will just run in a circle. Make this function smarter!
-        self.orientation += degree
-        self.orientation %= 360
+        # When the turtle rotates the same amount each turn,  it will just run in a circle. Make this function smarter!
+        prey_position = positions[0]
+
+        self.setheading(prey_position)
+        self.forward(3)
         # print(self.orientation)
-        return degree
+
+
 
 
 #  Insert the code of your sparring partner's turtle class here:
@@ -118,15 +160,13 @@ class PlayerName1(turtle.Turtle):
 
 
 # change these global constants only for debugging purposes:
-MAX_TURNS = 100       # Maximum number of turns in a hunt.                           In competition: probably 200.
-ROUNDS = 1            # Each player plays the prey this often.                       In competition: probably 10.
-STEP_SIZE = 3         # Distance each turtle moves in one turn.                      In competition: probably 3.
-SPEED = 0             # Fastest: 10, slowest: 1, max speed: 0.                       In competition: probably 0.
+MAX_TURNS = 100  # Maximum number of turns in a hunt.                           In competition: probably 200.
+ROUNDS = 1  # Each player plays the prey this often.                       In competition: probably 10.
+STEP_SIZE = 3  # Distance each turtle moves in one turn.                      In competition: probably 3.
+SPEED = 0  # Fastest: 10, slowest: 1, max speed: 0.                       In competition: probably 0.
 CAUGHT_DISTANCE = 10  # Hunt is over, when a hunter is nearer to the prey than that. In competition: probably 10.
-
 
 random.seed(2)  # use seed() if you want reproducible random numbers for debugging purposes. You may change the argument of seed().
 
-
-class1 = PlayerName1  # (red prey) Replace PlayerName1 by your own class name here.
-class2 = PlayerName1  # (green prey) For testing your code, replace PlayerName1 by your own class name here. Later replace this by your sparring partner's class name.
+class1 = Prey  # (red prey) Replace PlayerName1 by your own class name here.
+class2 = Prey  # (green prey) For testing your code, replace PlayerName1 by your own class name here. Later replace this by your sparring partner's class name.
